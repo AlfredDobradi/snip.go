d014f74191c5d3838881dc5d824df6f0bb848773
Alfred Dobradi <alfred@exads.com>
2017-06-14 16:16:43 +0100

    Refactor upload()

--- snip.go
+++ snip.go
@@ -41,9 +41,33 @@ func createBodyBuffer(body interface{}) (*bytes.Buffer, error) {
 	return bodyBuf, nil
 }
 
-// func makeRequest(endpoint string, body *bytes.Buffer, headers map[string]string) {
+func makeRequest(endpoint string, body *bytes.Buffer, headers map[string]string) (map[string]interface{}, string) {
+	url := "https://api.github.com" + endpoint
+	req, sendReqErr := http.NewRequest("POST", url, body)
+	panicOnError(sendReqErr)
 
-// }
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	respBody, sendResErr := ioutil.ReadAll(resp.Body)
+	panicOnError(sendResErr)
+
+	var bodyObject map[string]interface{}
+
+	respBodyErr := json.Unmarshal(respBody, &bodyObject)
+	panicOnError(respBodyErr)
+
+	return bodyObject, resp.Status
+}
 
 func panicOnError(err error) {
 	if err != nil {
@@ -178,44 +202,23 @@ func upload(files []string) {
 	panicOnError(bodyErr)
 
 	// POST /gists
-	url := "https://api.github.com/gists"
-	// REFACTOR START
-	req, sendReqErr := http.NewRequest("POST", url, gistJSON)
-	panicOnError(sendReqErr)
-	req.Header.Set("Accept", "application/vnd.github.v3+json")
-	req.Header.Set("Content-Type", "application/json")
+	endpoint := "/gists"
+	headers := make(map[string]string)
+
+	headers["Accept"] = "application/vnd.github.v3+json"
+	headers["Content-Type"] = "application/json"
 
 	if token != "" {
-		req.Header.Set("Authorization", fmt.Sprintf("token %s", token))
+		headers["Authorization"] = fmt.Sprintf("token %s", token)
 	}
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-
-	body, sendResErr := ioutil.ReadAll(resp.Body)
-	panicOnError(sendResErr)
-
-	var bodyObject interface{}
-	respBodyErr := json.Unmarshal(body, &bodyObject)
-	panicOnError(respBodyErr)
-
-	// REFACTOR END
-	// function should return map[string]interface{}
-
-	m := bodyObject.(map[string]interface{})
-
-	if resp.Status == "200" || resp.Status == "201 Created" {
-		fmt.Println(m["html_url"])
+	respBody, status := makeRequest(endpoint, gistJSON, headers)
+	if status == "200 OK" || status == "201 Created" {
+		fmt.Println(respBody["html_url"])
 	} else {
-		fmt.Println("response Status:", resp.Status)
-		fmt.Println(m)
+		fmt.Println("response Status:", status)
+		fmt.Println(respBody)
 	}
-
 }
 
 func main() {
